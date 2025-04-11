ec7df957b4e4b3936f3847616c3a1614b0da73d5
c-wann <free2737@gmail.com>
2025-04-11 18:24:20 +0800

    fix: update button styles for improved visibility and interaction

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <h1 class="text-[40vw] font-bold leading-none">{{ countdown }}</h1>
       <h3 class="text-3xl mt-4">{{ shootingGroup }} Shooting</h3>
       <p class="absolute top-8 left-8 text-xl">{{ currentTime }}</p>
-      <button type="button" @click="startCountdown(120)" class="mt-8 text-2xl px-8 py-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+      <button type="button" @click="startCountdown(120)" class="mt-8 text-2xl px-8 py-4 bg-transparent text-white rounded-lg hover:bg-blue-500 transition-colors">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
           <path fill-rule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clip-rule="evenodd" />
         </svg>
