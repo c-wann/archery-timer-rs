a76231b13892d3eb52909565c840785d8931e17a
c-wann <free2737@gmail.com>
2025-04-11 17:45:16 +0800

    fix: update output structure in rsbuild.config.mjs and adjust asset paths in index.html

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginVue } from '@rsbuild/plugin-vue';
 
 export default defineConfig({
-  assetPrefix: '/archery-timer-rs/',
-  base: "/archery-timer-rs/",
+  output: {
+    assetPrefix: '/archery-timer-rs/',
+    base: "/archery-timer-rs/",
+  },
   plugins: [pluginVue()],
 });
